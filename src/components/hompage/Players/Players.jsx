d7d650b892a64c61062dea8c5db6f222b1bcb332
Md. Amirul Islam <amirulislam.office28@gmail.com>
2026-03-27 11:26:08 +0600

    done creating section toggling and state management

--- src/components/hompage/Players/Players.jsx
+++ src/components/hompage/Players/Players.jsx
@@ -1,19 +1,27 @@
-import React, { use } from 'react';
+import { use, useState } from 'react';
 import AvailablePlayers from './AvailablePlayers/AvailablePlayers';
+import SelectedPlayers from './SelectedPlayers/SelectedPlayers';
 
 const Players = ({ playersPromise }) => {
     const playersData = use(playersPromise);
+    const [selectedType, setSelectedType] = useState("available");
     return (
         <>
         <div className="mt-10 flex justify-between">
-            <p className='font-bold text-2xl'>Available Players</p>
+                {selectedType === "available" ? <h2 className='font-bold text-2xl'>Available Players</h2> : <h2 className='font-bold text-2xl'>Selected Players (0 / 6)</h2>}
             <div>
-                <button className='btn btn-success'>Available</button>
-                <button className="btn">Selected</button>
+                    <button
+                    onClick={() => setSelectedType("available")}
+                    className={`btn ${selectedType === "available" ? "bg-[#E7FE29]" : ""} rounded-r-none rounded-l-xl`}>Available</button>
+                    <button
+                    onClick={() => setSelectedType("selected")}
+                    className={`btn ${selectedType === "selected" ? "bg-[#E7FE29]" : ""} rounded-l-none rounded-r-xl`}>Selected <span>0</span></button>
             </div>
             </div>
         <div className='mt-8'>
-            <AvailablePlayers Players = {playersData}></AvailablePlayers>
+            
+            
+                {selectedType === "available" ? <AvailablePlayers Players={playersData}></AvailablePlayers> : <SelectedPlayers></SelectedPlayers>}
         </div>
         </>
     );
---
+++ src/components/hompage/Players/SelectedPlayers/SelectedPlayers.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SelectedPlayers = () => {
+    return (
+        <div>Selected Players</div>
+    );
+};
+
+export default SelectedPlayers;
